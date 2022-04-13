94ea0f1f1e2d3c0a0b93529a90cf7269d01b21e8
Gustavo de Biagi Alves <agrobiagi@gmail.com>
2022-04-13 20:25:37 -0300

    aula variaveis

--- modulo1/projeto-onboarding/aula-variaveis/template_javascript/index.js
+++ modulo1/projeto-onboarding/aula-variaveis/template_javascript/index.js
@@ -16,12 +16,12 @@ console.log( alert (`multiplicação ${Number(variavel1)* Number(variavel2)} `))
     //VARIÁVEIS
     //variavel1, variavel2, variavel3, variavel4, variavel5, variavel6
     //
-let variavel1 = prompt("Primeiro Número")
-let variavel2 = prompt ("Segundo Número")
+const variavel1 = prompt("Primeiro Número")
+const variavel2 = prompt ("Segundo Número")
     //
     //
-const variavel3 = (Number(variavel1) + Number(variavel2))
-const variavel4 = (Number(variavel1) * Number(variavel2))
+let variavel3 = (Number(variavel1) + Number(variavel2))
+let variavel4 = (Number(variavel1) * Number(variavel2))
     //
     //
 console.log (alert( variavel3  + ("   Soma = X") ))
@@ -30,7 +30,6 @@ console.log (alert (variavel4  + ("    Multiplicação = Y")   ))
     // 
 let variavel5  = Boolean(  prompt("Gostaria de Voltar?  Sim / Não "  )) = (Sim=true,Não=false)  
 console.log (alert ( variavel5 ))
-let variavel6 = Boolean (variavel5) (prompt(variavel5)) 
     //
     //
     //    
