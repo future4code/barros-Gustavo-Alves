0b0fb4c2a9075450ab1e2d2c72fc7f6190002537
Gustavo de Biagi Alves <agrobiagi@gmail.com>
2022-04-13 18:38:49 -0300

    aula-variaveis

--- modulo1/projeto-onboarding/aula-variaveis/template_javascript/index.js
+++ modulo1/projeto-onboarding/aula-variaveis/template_javascript/index.js
@@ -1,8 +1,36 @@
-
-let a = 10
-let b = 10
-
-console.log(b)
-
-b = 5
-console.log(a, b)
+//INÍCIO
+    //VARIÁVEIS
+    //variavel1, variavel2, 
+    //  
+    //
+/*   let variavel1 = prompt("Primeiro Número")
+ let variavel2 = prompt("Segundo Número")
+console.log( alert (`soma ${Number(variavel1)+Number(variavel2)} `))
+console.log( alert (`multiplicação ${Number(variavel1)* Number(variavel2)} `))  */
+    //
+    //
+//FIM
+    //
+    //
+//INÍCIO
+    //VARIÁVEIS
+    //variavel1, variavel2, variavel3, variavel4, variavel5, variavel6
+    //
+let variavel1 = prompt("Primeiro Número")
+let variavel2 = prompt ("Segundo Número")
+    //
+    //
+const variavel3 = (Number(variavel1) + Number(variavel2))
+const variavel4 = (Number(variavel1) * Number(variavel2))
+    //
+    //
+console.log (alert( variavel3  + ("   Soma = X") ))
+console.log (alert (variavel4  + ("    Multiplicação = Y")   ))
+    //
+    // 
+let variavel5  = Boolean(  prompt("Gostaria de Voltar?  Sim / Não "  )) = (Sim=true,Não=false)  
+console.log (alert ( variavel5 ))
+let variavel6 = Boolean (variavel5) (prompt(variavel5)) 
+    //
+    //    
+// FIM 
