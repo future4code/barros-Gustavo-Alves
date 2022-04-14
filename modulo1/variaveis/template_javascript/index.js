7b0895e9399c395f36a0696b4d3e8de23364842f
Gustavo de Biagi Alves <agrobiagi@gmail.com>
2022-04-13 22:56:06 -0300

    aula operadores

---
+++ modulo1/variaveis/template_javascript/index.js
@@ -0,0 +1,36 @@
+//INÍCIO
+    //VARIÁVEIS
+    //variavel1, variavel2, 
+    //  
+    //
+/*   let variavel1 = prompt("Primeiro Número")
+ let variavel2 = prompt("Segundo Número")
+console.log( alert (`soma ${Number(variavel1)+Number(variavel2)} `))
+console.log( alert (`multiplicação ${Number(variavel1)* Number(variavel2)} `))  */
+    //
+    //
+//FIM
+    //
+    //
+//INÍCIO
+    //VARIÁVEIS
+    //variavel1, variavel2, variavel3, variavel4, variavel5, variavel6
+    //
+    const variavel1 = prompt("Primeiro Número")
+    const variavel2 = prompt ("Segundo Número")
+        //
+        //
+    let variavel3 = (Number(variavel1) + Number(variavel2))
+    let variavel4 = (Number(variavel1) * Number(variavel2))
+        //
+        //
+    console.log (alert( variavel3  + ("   Soma = X") ))
+    console.log (alert (variavel4  + ("    Multiplicação = Y")   ))
+        //
+        // 
+    let variavel5  = Boolean(  prompt("Gostaria de Voltar?  Sim / Não "  )) = (Sim=true,Não=false)  
+    console.log (alert ( variavel5 ))
+        //
+        //
+        //    
+    // FIM 
